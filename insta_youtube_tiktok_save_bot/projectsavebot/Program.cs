b8d59e53dd71c3156767e3006b99ce3c94739f3e
AL1JONOV <alijonov0608@gmail.com>
2022-06-17 15:26:07 +0500

    reply method  message added

---
+++ insta_youtube_tiktok_save_bot/projectsavebot/Program.cs
@@ -0,0 +1,42 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace allsavebot
+{
+    class Program
+    {
+        private static string token = "5589132832:AAFDZlIuIAteCUFNDaTeweCyu62A3ApYVxM";
+        private static TelegramBotClient client;
+
+        static void Main(string[] args)
+        {
+            client = new TelegramBotClient(token);
+            client.StartReceiving();
+            client.OnMessage += OnMessageHandler;
+            Console.ReadLine();
+            client.StopReceiving();
+            //Console.WriteLine("Assalomu Alaykum : Hi every one: ");
+
+        }
+
+        private static async void OnMessageHandler(object? sender, MessageEventArgs e)
+        {
+
+            var message = e.Message;
+            if (message.Text != null)
+            {
+                Console.WriteLine($"Salom => {message.Text}");
+                await client.SendTextMessageAsync(
+                    message.Chat.Id,
+                    message.Text
+                   // replyToMessageId: message.MessageId
+                    );
+            }
+        }
+
+
+    }
+}
